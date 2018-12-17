96d8939238b793d6d5e4ecb1766b5cc5d70d9d3a
Thomas Sauvajon <thomas@sauvajon.tech>
2018-12-17 16:57:35 +0100

    add bubble sort

---
+++ bubblesort.js
@@ -0,0 +1,19 @@
+function bubbleSort(array) {
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j+1]) {
+        [array[j+1], array[j]] = [array[j], array[j+1]]
+      }
+    }
+  }
+  return array;
+}
+
+var testBubblesort = () => test(bubbleSort)
+var benchmarkBubblesort = () => benchmark(bubbleSort)
+
+if (window.benchmarkFns) {
+  window.benchmarkFns = [...window.benchmarkFns, bubbleSort]
+} else {
+  window.benchmarkFns = [bubbleSort]
+}
